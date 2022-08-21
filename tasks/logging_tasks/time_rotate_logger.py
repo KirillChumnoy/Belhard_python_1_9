d8712108b715967d5ca05ba5a7d132a344ddc0cf
Kirillchumnoy <rebilion@gmail.com>
2022-08-21 11:33:20 +0300

    done_time_rotate_logger.py

--- tasks/logging_tasks/time_rotate_logger.py
+++ tasks/logging_tasks/time_rotate_logger.py
@@ -5,3 +5,15 @@
 Настроить, чтобы вывод шел в файл и в консоль.
 Настроить ротацию файла лога по времени (полночь).
 """
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="log_journal.txt"
+)
+my_loger = logging.getLogger(__name__)
+write_to_journal = logging.StreamHandler()
+rotate_my_journals = TimedRotatingFileHandler("log_journal.txt", when="midnight")
+my_loger.addHandler(write_to_journal)
+my_loger.addHandler(rotate_my_journals)
